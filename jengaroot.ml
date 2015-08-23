99b947e85227f6e2fe4e5ba9195d8dc97c322992
Alexandru Scvortov <code@scvalex.net>
2015-08-23 16:03:37 +0100

    support for standard ocaml libs

--- jengaroot.ml
+++ jengaroot.ml
@@ -14,6 +14,8 @@ let (^/) dir name = Path.relative ~dir name;;
 
 let (/^) = (^/);;
 
+let ocamlfind_wrap = Path.to_absolute_string (Path.the_root ^/ "ocamlfind-wrap")
+
 let basename = Path.basename;;
 
 let nothing_to_build_rules ~dir =
@@ -138,7 +140,7 @@ module Ocaml = struct
           ["-cclib"; "-l" ^ foreign_library])
     in
     let warning_args =
-      let ignored_warnings = ["4"; "23"; "41"; "44"; "45"] in
+      let ignored_warnings = ["4"; "23"; "40"; "41"; "42"; "44"; "45"] in
       let ignored_warnings =
         if allow_unused_opens then "33" :: ignored_warnings else ignored_warnings
       in
@@ -147,9 +149,12 @@ module Ocaml = struct
     let syntax_args =
       ["-syntax"; "camlp4o"]
     in
-    Action.process ~dir ~prog:"ocamlfind"
+    let cmt_args =
+      ["-bin-annot"; "-g"]
+    in
+    Action.process ~dir ~prog:ocamlfind_wrap
       ~args:(List.concat [ ["ocamlopt"]; args; packages_args; pack_args; include_args
-                         ; foreign_args; warning_args; syntax_args
+                         ; foreign_args; warning_args; syntax_args; cmt_args
                          ])
   ;;
 
@@ -160,7 +165,7 @@ module Ocaml = struct
     let syntax_args =
       ["-syntax"; "camlp4o"]
     in
-    Action.process ~dir ~prog:"ocamlfind"
+    Action.process ~dir ~prog:ocamlfind_wrap
       ~args:(List.concat [ ["ocamldep"]; args; packages_args; syntax_args
                          ])
   ;;
@@ -203,11 +208,14 @@ module Ocaml = struct
     val external_libraries : t -> string list
 
     val foreign_libraries : t -> string list
+
+    val classic_libs : t -> string list
   end = struct
     type t = {
       libraries          : string list sexp_option;
       external_libraries : string list sexp_option;
       foreign_libraries  : string list sexp_option;
+      classic_libs      : string list sexp_option;
     } with sexp
 
     let load ~dir =
@@ -240,6 +248,11 @@ module Ocaml = struct
     let foreign_libraries t =
       Option.value ~default:[] t.foreign_libraries
     ;;
+
+    let classic_libs t =
+      Option.value ~default:[] t.classic_libs
+    ;;
+
   end
 
   let ocamldep_deps ~dir ~source =
@@ -368,7 +381,7 @@ module Ocaml = struct
      `External external_libraries)
   ;;
 
-  let link_exe_rule ~dir ~libraries ~external_libraries ~foreign_libraries ~exe names =
+  let link_exe_rule ~dir ~libraries ~external_libraries ~foreign_libraries ~classic_libs ~exe names =
     let link_exe =
       Dep.all_unit (compiled_files_for ~dir names)
       *>>= fun () ->
@@ -391,7 +404,8 @@ module Ocaml = struct
         ~for_pack:None ~allow_unused_opens:false
         ~include_dirs:(Liblinks.include_dirs ~dir libraries)
         ~args:(List.concat
-                 [ [ "-linkpkg"
+                 [ List.map classic_libs ~f:(fun m -> m ^ ".cmxa")
+                 ; [ "-linkpkg"
                    ; "-o"; basename exe
                    ]
                  ; List.map libraries ~f:(fun lib -> Lib.suffixed_name lib ^ ".cmxa")
@@ -498,12 +512,13 @@ module Ocaml = struct
       let external_libraries = Mlbuild.external_libraries mlbuild in
       let foreign_libraries = Mlbuild.foreign_libraries mlbuild in
       let libraries = Mlbuild.libraries mlbuild in
+      let classic_libs = Mlbuild.classic_libs mlbuild in
       let exe = dir ^/ (basename dir) ^ ".exe" in
       compile_mls_in_dir_rules ~dir ~libraries ~external_libraries ~for_pack:None
       *>>| fun (names, compile_mls_rules) ->
       List.concat
         [ [ Rule.default ~dir [Dep.path exe]
-          ; link_exe_rule ~dir ~libraries ~external_libraries ~foreign_libraries ~exe names
+          ; link_exe_rule ~dir ~libraries ~external_libraries ~foreign_libraries ~classic_libs ~exe names
           ]
         ; compile_mls_rules
         ])
